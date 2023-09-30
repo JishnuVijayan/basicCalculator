d47dd500102255a01918edc5b4e727c0a05a4e61
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2023-09-30 17:55:07 +0530

    Calculator is Working.
    Using react useState() and proper functions the calculator is working properly.

--- basiccalculator/src/Calculator.js
+++ basiccalculator/src/Calculator.js
@@ -1,14 +1,60 @@
 import Box from "@mui/material/Box";
 import "./calculator.css";
 import Button from "@mui/material/Button";
+import { useState } from "react";
 
 export default function Calculator() {
+  const [calcValue, setCalcValue] = useState("");
+  const [result, setResult] = useState("");
+  const operators = ["+", "-", "*", "/"];
+  const decimalPoint = ["."];
+  const updateValue = (value) => {
+    if (
+      (operators.includes(value) && calcValue === "") ||
+      (operators.includes(value) && operators.includes(calcValue.slice(-1)))
+    )
+      return;
+    else setCalcValue(calcValue + value);
+
+    if (!operators.includes(value)) {
+      setResult(eval(calcValue + value).toString());
+    }
+  };
+  const calculation = () => {
+    setCalcValue(eval(calcValue).toString());
+  };
+  const decimal = (value) => {
+    if (value === ".") setCalcValue(calcValue + value);
+  };
+  const deleteLast = () => {
+    if (calcValue == " ") {
+      return;
+    }
+
+    const Value = calcValue.slice(0, -1);
+    setCalcValue(Value);
+  };
+  const clear = () => {
+    setCalcValue("");
+  };
   return (
     <div className="main">
       <h1 style={{ fontSize: 70 }}>CALCULATOR</h1>
       <Box className="Box">
         <div className="textField">
-          <span> 0</span>
+          <span
+            style={{
+              color: "#ffffff",
+              justifyContent: "flex-end",
+              alignItems: "baseline",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              fontSize: 25,
+            }}
+          >
+            {calcValue ? calcValue : 0}
+          </span>
         </div>
         <div className="hr"></div>
         <div className="row">
@@ -20,6 +66,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("1")}
           >
             <b>1</b>
           </Button>
@@ -31,6 +78,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("2")}
           >
             <b>2</b>
           </Button>
@@ -42,6 +90,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("3")}
           >
             <b>3</b>
           </Button>
@@ -55,6 +104,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("4")}
           >
             <b>4</b>
           </Button>
@@ -66,6 +116,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("5")}
           >
             <b>5</b>
           </Button>
@@ -77,6 +128,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("6")}
           >
             <b>6</b>
           </Button>
@@ -90,6 +142,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("7")}
           >
             <b>7</b>
           </Button>
@@ -101,6 +154,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("8")}
           >
             <b>8</b>
           </Button>
@@ -112,6 +166,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("9")}
           >
             <b>9</b>
           </Button>
@@ -125,6 +180,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => deleteLast()}
           >
             <b>DELETE</b>
           </Button>
@@ -136,6 +192,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("0")}
           >
             <b>0</b>
           </Button>
@@ -147,6 +204,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => calculation()}
           >
             <b>=</b>
           </Button>
@@ -161,6 +219,7 @@ export default function Calculator() {
               width: 40,
               fontWeight: "20",
             }}
+            onClick={() => updateValue("+")}
           >
             <b>+</b>
           </Button>
@@ -172,6 +231,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("-")}
           >
             <b>-</b>
           </Button>
@@ -183,6 +243,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("*")}
           >
             <b>*</b>
           </Button>
@@ -196,6 +257,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => updateValue("/")}
           >
             <b>/</b>
           </Button>
@@ -208,6 +270,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => decimal(".")}
           >
             <b>.</b>
           </Button>
@@ -219,6 +282,7 @@ export default function Calculator() {
               height: 50,
               width: 40,
             }}
+            onClick={() => clear()}
           >
             <b>CLEAR</b>
           </Button>
